0155f9028f4b5ab8b23a9bb4fce09f035ef8da2f
Stamballin <matt.stambaugh@hotmail.com>
2025-05-24 16:29:02 -0500

    update book path

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
+import sys
+
 from stats import get_num_words, get_chars_dict
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
 
-    print(f"{num_words} words found in the document")
-    print("\nCharacter counts:")
-    for char, count in chars_dict.items():
-        print(f"'{char}': {count}")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words in the document")
+    print("--------- Character Count -------")
+        
+
+    for char, count in sorted(chars_dict.items(), key=lambda x: x[1], reverse = True):
+        if char.isalpha():
+           print(f"{char}: {count}")
+           
+   
+    print("============= END ===============")
 
 def get_book_text(path):
     with open(path) as f:
